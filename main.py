455edb75a737142e216810c5889564bab0f93332
adityamwagh <adityamwagh@gmail.com>
2022-03-10 00:27:34 -0500

    refactored main function

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import argparse
 import os
-import time
-from time import sleep
 
 import numpy as np
 import torch
@@ -14,41 +12,48 @@ from torch.utils.data import DataLoader
 from project1_model import project1_model
 
 
-
 # Defining the Training Loop
-def train_test_model(epochs, train_loader, test_loader, model, loss_fn, optimizer, train_loss_values, train_accuracy):
+def train_test_model(epochs, train_loader, test_loader, model, loss_fn, optimizer):
 
-    model.train()
     for epoch in range(1, epochs + 1):
-
-        running_loss = 0
+        """
+        Model training
+        """
+        model.train()
+        running_train_loss = 0
         correct = 0
         total = 0
 
         for imgs, labels in train_loader:
 
             X, y = imgs.to(device), labels.to(device)
-            pred = model(X)
-            loss = loss_fn(pred, y)
+            train_pred = model(X)
+            train_loss = loss_fn(train_pred, y)
 
             # Back prop
             optimizer.zero_grad()
-            loss.backward()
+            train_loss.backward()
             optimizer.step()
 
-            running_loss += loss.item()
-            _, predicted = pred.max(1)
+            running_train_loss += train_loss.item()
+            _, predicted = train_pred.max(1)
             total += labels.size(0)
             correct += predicted.eq(labels.to(device)).sum().item()
 
-        train_loss = running_loss / len(train_dataloader)
-        train_loss_values.append(train_loss)
-        accu = 100.0 * correct / total
-        train_accuracy.append(accu)
+        epoch_train_loss = running_train_loss / len(train_dataloader)
+        train_loss_values.append(epoch_train_loss)
+        epoch_train_accuracy = 100.0 * correct / total
+        train_accuracy_values.append(epoch_train_accuracy)
 
+        # print accuracy and loss at every 10 epochs
+        if epoch % 10 == 0:
+            print(f"Epoch {epoch}/{epochs} | Train Loss: {epoch_train_loss} | Train Accuracy: {epoch_train_accuracy} %")
+
+        """
+        Model testing
+        """
         model.eval()
-
-        running_loss = 0.0
+        running_test_loss = 0.0
         correct = 0
         total = 0
 
@@ -59,33 +64,41 @@ def train_test_model(epochs, train_loader, test_loader, model, loss_fn, optimize
 
             # compute test Loss
             loss = loss_fn(pred, y)
-            running_loss += loss.item()
+            running_test_loss += loss.item()
             _, predicted = torch.max(pred.data, 1)
             total += labels.size(0)
 
             # correct += (predicted == labels).sum().item()
             correct += predicted.eq(labels.to(device)).sum().item()
 
-        test_loss = running_loss / len(test_dataloader)
-        test_loss_values.append(test_loss)
-        accu = 100.0 * correct / total
-        test_accuracy.append(accu)
+        epoch_test_loss = running_test_loss / len(test_dataloader)
+        test_loss_values.append(epoch_test_loss)
+        epoch_test_accuracy = 100.0 * correct / total
+        test_accuracy_values.append(epoch_test_accuracy)
 
+        # print accuracy and loss at every 10 epochs
+        if epoch % 10 == 0:
+            print(f"Epoch {epoch}/{epochs} | Test Loss: {epoch_test_loss} | Test Accuracy: {epoch_test_accuracy} %")
 
 def select_optimiser(argument, model):
 
+    # stochastic gradient descent
     if argument == "sgd":
-        return optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=args.weight_decay)
+        return optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
 
+    # stochastic gradient descent with nesterov correction
     if argument == "sgd_nest":
-        return optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=args.weight_decay, nesterov=True)
+        return optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay, nesterov=True)
 
+    # adaptive gradient descent
     if argument == "adagrad":
         return optim.Adagrad(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
 
+    # adadelta
     if argument == "adadelta":
         return optim.Adadelta(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
 
+    # adam optimizer
     if argument == "adam":
         return optim.Adam(model.parameters(), lr=0.001, weight_decay=args.weight_decay)
 
@@ -93,47 +106,52 @@ def select_optimiser(argument, model):
 if __name__ == "__main__":
 
     """
-    ARGUMENT PROVISION
+    Provision for arguments.
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("-en", "--experiment_number", type=str, required=True, help="number to track the different experiments")
     parser.add_argument("-o", "--optimiser", type=str, required=True, help="optimizer for training")
-    parser.add_argument("-d", "--device", type=str, required=False, default="gpu", help="device to train on")
-    parser.add_argument("-e", "--epochs", type=int, required=False, default=120, help="number of epochs to train for")
-    parser.add_argument("-lr","--learning-rate",type=float,required=False,default=0.1,help="learning rate for the optimizer")
-    parser.add_argument("-m", "--momentum", type=float, required=False, default=0.9, help="momentum value for optimizer if applicable")
-    parser.add_argument(
-        "-wd",
-        "--weight-decay",
-        type=float,
-        required=False,
-        default=5e-4,
-        help="weight decay value for the optimizer if applicable",
-    )
+    parser.add_argument("-d", "--device", type=str, required=False, default="gpu", help="device to train on, default is gpu")
+    parser.add_argument("-e", "--epochs", type=int, required=False, default=120, help="number of epochs to train for, default is 120")
+    parser.add_argument("-lr", "--learning-rate", type=float, required=False, default=0.1, help="learning rate for the optimizer, default is 0.1")
+    parser.add_argument("-m", "--momentum", type=float, required=False, default=0.9, help="momentum value for optimizer if applicable, default is 0.9")
+    parser.add_argument("-wd", "--weight-decay", type=float, required=False, default=5e-4, help="weight decay value for the optimizer if applicable, default is 5e-4")
     parser.add_argument("-dp", "--data-path", type=str, required=True, help="path to the dataset")
     parser.add_argument("-b", "--blocks", nargs=4, required=True, type=int, help="number of blocks in each layer")
     parser.add_argument("-c", "--channels", nargs=4, required=True, type=int, help="number of channels in each layer")
     args = parser.parse_args()
 
     """
-    HYPERPARAMETERS
+    Hyperparameters
     """
+
+    # path to the dataset
+    data_path = args.data_path
+
+    # number of epochs
+    epochs = args.epochs
+
+    # block size for resnet
+    blocks = args.blocks
+
+    # number of convolutional filters in each conv layer
+    channels = args.channels
+
+    # device to train on
     if args.device == "gpu" and torch.cuda.is_available() == True:
         device = "cuda"
     else:
         device = "cpu"
 
-    data_path = args.data_path
-    epochs = args.epochs
-    blocks = args.blocks
-    channels = args.channels
-
-    resnet_model = project1_model(blocks, channels).to(device)
-    optimizer = select_optimiser(args.optimiser, resnet_model)
+    # loss function initialization
     loss = nn.CrossEntropyLoss()
 
+    # model initialization
+    resnet_model = project1_model(blocks, channels).to(device)
+    optimizer = select_optimiser(args.optimiser, resnet_model)
+
     """
-    DATA RELATED STUFF
+    Data Related Stuff
     """
     mean = (0.4914, 0.4822, 0.4465)
     std = (0.2023, 0.1994, 0.2010)
@@ -155,34 +173,34 @@ if __name__ == "__main__":
     test_dataloader = DataLoader(test_data, batch_size=64, shuffle=False, num_workers=2)
 
     """
-    MODEL TRAINING AND EVALUATION
+    Model Training And Evaluation
     """
     print("Started Training\n")
 
     train_loss_values = []
     test_loss_values = []
-    train_accuracy = []
-    test_accuracy = []
+    train_accuracy_values = []
+    test_accuracy_values = []
 
-    train_test_model(epochs, train_dataloader, test_dataloader, resnet_model, loss, optimizer, train_loss_values, train_accuracy)
+    train_test_model(epochs, train_dataloader, test_dataloader, resnet_model, loss, optimizer)
 
     print("Finished Training\n")
 
-    ## Save the train_loss_values and test_loss_values us np.save function
-
+    # save the training & testing loss & accuracy to the disk
     os.makedirs(os.path.join(os.getcwd(), "metrics"), exist_ok=True)
-
     np.save(os.path.join("metrics", args.experiment_number + "_train_loss.npy"), train_loss_values)
     np.save(os.path.join("metrics", args.experiment_number + "_test_loss.npy"), test_loss_values)
-    np.save(os.path.join("metrics", args.experiment_number + "_train_accuracy.npy"), train_accuracy)
-    np.save(os.path.join("metrics", args.experiment_number + "_test_accuracy.npy"), test_accuracy)
+    np.save(os.path.join("metrics", args.experiment_number + "_train_accuracy.npy"), train_accuracy_values)
+    np.save(os.path.join("metrics", args.experiment_number + "_test_accuracy.npy"), test_accuracy_values)
 
+    #  save model weights to weights folder
     os.makedirs(os.path.join(os.getcwd(), "weights"), exist_ok=True)
-    PATH_MODEL_WEIGHTS = os.path.join("weights", args.experiment_number + "_weight.pth")
+    PATH_MODEL_WEIGHTS = os.path.join("weights", args.experiment_number + "_weights.pth")
     torch.save(resnet_model.state_dict(), PATH_MODEL_WEIGHTS)
+    print(f"Saved model weights to {PATH_MODEL_WEIGHTS}")
 
-    print("Model Saved\n")
-    print(f"Final Training Accuracy: {train_accuracy[-1]: .3f}| Final Test Accuracy: {test_accuracy[-1]: .3f}\n")
+    # print final results and settings 
+    print(f"Final Training Accuracy: {train_accuracy_values[-1]: .3f} % | Final Test Accuracy: {test_accuracy_values[-1]: .3f} %\n")
     print("This accuracy is achieved with the following Settings")
     print(f"Number of Blocks in each layer: {blocks}\n Number of Channels in each layer: {channels}")
     print(f"Optimiser: {args.optimiser}\n Learning Rate: {args.learning_rate}")
